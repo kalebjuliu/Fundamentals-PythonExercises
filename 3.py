70a6486a5b6f4052588cdf6bc40f839948104b68
Kaleb Juliu <kalebjuliu@gmail.com>
2019-05-24 19:53:24 +0700

    Delete 3.py

--- 3.py
+++
@@ -1,9 +0,0 @@
-
-
-# Exercise 3 : Character Input
-current_age = int(input("How old are you? "))
-age_diff = 100 - current_age
-year_diff = 2019 + age_diff
-
-print(f"At 100 years old the year will be at {year_diff}")
-input("Press enter to exit")
