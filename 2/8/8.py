a7589e5690b7ee5efbf3cf944cdc299610400e5b
Kaleb Juliu <kalebjuliu@gmail.com>
2019-05-29 18:48:17 +0700

    Add files via upload

--- 2/8/8.py
+++ 2/8/8.py
@@ -20,22 +20,32 @@ def main(text_file):
 		word_list.append(elem)
 	return word_list
 
+
 word = main(all_text)
-
-play = True
-while play:
-	guessed = "_ " * len(word)
-	letter = str(input('Enter a letter'))
-	guessed = list(guessed)
-
-	if letter.upper() in word:
+guessed = "_" * len(word)
+guessed = list(guessed)
+lstGuessed = []
+letter = input("guess letter: ")
+while True:
+	if letter.upper() in lstGuessed:
+		letter = ''
+		print("Already guessed!!")
+	elif letter.upper() in word:
 		index = word.index(letter.upper())
 		guessed[index] = letter.upper()
+		word[index] = '_'
+	else:
+		print(''.join(guessed))
+		if letter is not '':
+			lstGuessed.append(letter.upper())
+		letter = input("guess letter: ")
+
+	if '_' not in guessed:
+		print("You won!!")
+		break
 
 
 
 
-print(word)
-input("Enter to exit")
 
 
